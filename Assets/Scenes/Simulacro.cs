1950adcc8fa14e74a47954eefeb7812c56e17119
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-12 08:38:45 -0300

    Tema1terminado

--- Assets/Scenes/Simulacro.cs
+++ Assets/Scenes/Simulacro.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class Simulacro : MonoBehaviour
 {
+    public float montototal;
+    float valordolar = 112;
+    float valoreuro = 122;
+    float valorreal = 24;
+    public string moneda;
+    float cantidadconvertida;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(montototal < 1000)
+        {
+            Debug.Log("El monto minimo es $1000");
+        }
+        else
+        {
+            switch (moneda)
+            {
+                case "D":
+                    cantidadconvertida = montototal / valordolar;
+                    Debug.Log(montototal + " pesos argentinos equivalen a " + cantidadconvertida + " Dolares");
+                    break;
+                case "E":
+                    cantidadconvertida = montototal / valoreuro;
+                    Debug.Log(montototal + " pesos argentinos equivalen a " + cantidadconvertida + " Euros");
+                    break;
+                case "R":
+                    cantidadconvertida = montototal / valorreal;
+                    Debug.Log(montototal + " pesos argentinos equivalen a " + cantidadconvertida + " Reales");
+                    break;
+                default:
+                    Debug.Log("Opcion de moneda extranjera no valida");
+                    break;
+            }
+        }
     }
 
     // Update is called once per frame
